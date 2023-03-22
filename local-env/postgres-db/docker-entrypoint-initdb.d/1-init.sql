238a484abb459d72184448fe661cde6b24adebbd
Barrett Kepler <45532267+kepler-br@users.noreply.github.com>
2023-03-22 21:57:34 +0300

    Add docker files for gateway, robot and neural text backend

--- local-env/postgres-db/docker-entrypoint-initdb.d/1-init.sql
+++ local-env/postgres-db/docker-entrypoint-initdb.d/1-init.sql
@@ -6,7 +6,7 @@ create database "omega";
 create user "nonroot" with password 'nonroot';
 
 grant all on database "omega" to "nonroot";
--- grant all on schema public to "nonroot";
+grant all on schema public to "nonroot";
 create schema gateway;
 grant all on schema gateway to "nonroot";
 create schema bot;
