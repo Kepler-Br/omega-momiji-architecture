6deb99e9346b1cb318a28f6fc4b5298e1a799c86
Barrett Kepler <45532267+kepler-br@users.noreply.github.com>
2024-02-27 07:03:08 +0000

    Changes to DB, docker-compose and gateway-contract

--- local-env/changelog/0.0.0/gateway/messages.sql
+++ local-env/changelog/0.0.0/gateway/messages.sql
@@ -4,13 +4,13 @@
 create table gateway.messages
 (
     id                         bigserial primary key,
-    "text"                     varchar null,
-    s3bucket                   varchar(100)                         null,
-    s3key                      varchar(100)                         null,
+    "text"                     varchar                              null,
+    s3_bucket                  varchar(100)                         null,
+    s3_key                     varchar(100)                         null,
     media_type                 varchar(15)                          null
         constraint chk_messages_media_type check (media_type in
-        ('STICKER', 'AUDIO', 'VOICE', 'PHOTO', 'VIDEO',
-        'GIF', 'VIDEO_NOTE')),
+                                                  ('STICKER', 'AUDIO', 'VOICE', 'PHOTO', 'VIDEO',
+                                                   'GIF', 'VIDEO_NOTE')),
     chat_id                    bigint                               not null
         constraint fk_messages_chats_id references gateway.chats (id),
     user_id                    bigint references gateway.users (id) not null
@@ -18,7 +18,8 @@ create table gateway.messages
     frontend                   varchar(100)                         not null,
     native_id                  varchar(100)                         not null,
     reply_to_message_native_id varchar(100),
-    created_at                 timestamp null default now()
+    created_at                 timestamp                            null default now(),
+    updated_at                 timestamp                            null default now()
 );
 
-create unique index uix_messages_native_id_frontend on gateway.messages (native_id, frontend);
+create unique index uix_messages_native_id_frontend on gateway.messages (native_id, chat_id, user_id, frontend);
--- local-env/changelog/0.0.0/gateway/users.sql
+++ local-env/changelog/0.0.0/gateway/users.sql
@@ -8,7 +8,8 @@ create table gateway.users
     fullname   varchar(1024) not null,
     frontend   varchar(100)  not null,
     native_id  varchar(100)  not null,
-    created_at timestamp null default now()
+    created_at timestamp     null default now(),
+    updated_at timestamp     null default now()
 );
 
 create unique index uix_users_native_id_frontend on gateway.users (native_id, frontend);
